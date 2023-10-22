f20df2728e8c0be9be170a299268a667c3c0ec42
stg-annon <14135675+stg-annon@users.noreply.github.com>
2023-10-22 14:24:00 -0400

    bump bundle

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-GITHUB_ROOT_URL = r"https://raw.githubusercontent.com/stg-annon/stash-userscripts/%%BRANCH%%/"
-BUNDLE_VERSION = "0.21.2"
+GITHUB_ROOT_URL = r"https://raw.githubusercontent.com/stg-annon/stash-userscripts/main/"
+BUNDLE_VERSION = "0.22.0"
 SERVER_URL = "http://localhost:9999"
 NAMESPACE = "https://github.com/stg-annon/stash-userscripts"
