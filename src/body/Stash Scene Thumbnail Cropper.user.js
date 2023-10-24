bae810e7cf4e7da8ef11a3785ff17133c6d7b972
feederbox826 <144178721+feederbox826@users.noreply.github.com>
2023-10-24 12:05:29 -0400

    update scene thumbnail cropper (#8)

--- src/body/Stash Scene Thumbnail Cropper.user.js
+++ src/body/Stash Scene Thumbnail Cropper.user.js
@@ -1,18 +1,3 @@
-// ==UserScript==
-// @name        Stash Scene Thumbnail Cropper
-// @namespace   https://github.com/stg-annon/stash-userscripts
-// @description Adds an image cropper to scene page
-// @version     0.1.0
-// @author      scruffynerf
-// @match       http://localhost:9999/*
-// @resource    IMPORTED_CSS https://raw.githubusercontent.com/fengyuanchen/cropperjs/main/dist/cropper.min.css
-// @grant       unsafeWindow
-// @grant       GM_getResourceText
-// @grant       GM_addStyle
-// @require     https://raw.githubusercontent.com/stg-annon/stash-userscripts/main/src/StashUserscriptLibrary.js
-// @require     https://raw.githubusercontent.com/fengyuanchen/cropperjs/main/dist/cropper.min.js
-// ==/UserScript==
-
 /* global Cropper */
 
 (function () {
---
+++ src/header/Stash Scene Thumbnail Cropper.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Stash Scene Thumbnail Cropper
+// @namespace   %NAMESPACE%
+// @description Adds an image cropper to scene page
+// @version     0.1.0
+// @author      scruffynerf
+// @match       %MATCHURL%
+// @resource    IMPORTED_CSS https://raw.githubusercontent.com/fengyuanchen/cropperjs/main/dist/cropper.min.css
+// @grant       unsafeWindow
+// @grant       GM_getResourceText
+// @grant       GM_addStyle
+// @require     %LIBRARYPATH%
+// @require     https://raw.githubusercontent.com/fengyuanchen/cropperjs/main/dist/cropper.min.js
+// @require     %FILEPATH%
+// ==/UserScript==
